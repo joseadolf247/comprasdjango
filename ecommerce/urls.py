c8edf85d5ee85701af13eda46fd7a22088c83617
JonaCarranza <jonathanyaelcarranza9@gmail.com>
2024-05-06 22:26:23 -0700

    parte del proyecto video 1

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,4 @@
-"""
-URL configuration for ecommerce project.
+"""ecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -17,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
+
+urlpatterns += static(settings.MEDIAS_URL, document_root=settings.MEDIA_ROOT)
